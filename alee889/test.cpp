781e43c33726f994dd992330c24d7808bca4973b
alison <alee889@binghamton.edu>
2021-03-30 02:15:27 -0400

    test file

---
+++ alee889/test.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+
+using namespace std;
+
+int main( int argc, char *argv[]) {
+  unsigned long long addr;
+  string behavior, line;
+  unsigned long long target;
+
+  ifstream infile( "test_input.txt" );
+  ofstream outfile( "test_output.txt" ) ;
+  while( getline( infile, line )) {
+    stringstream s( line );
+    s >> std::hex >> addr >> behavior >> std::hex >> target;
+    outfile << addr;
+    if(behavior == "T") {
+      outfile << " -> taken, ";
+    } 
+    else {
+      outfile << " -> not taken, ";
+    }
+    outfile << "target=" << target << endl;
+  }
+
+  return 0;
+}
