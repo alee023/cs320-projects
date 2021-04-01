f692d69ab64e1b47be72db1be542d75807085b29
Alison Lee <alee889@cs.binghamton.edu>
2021-04-01 13:53:08 -0400

    static working

--- alee889/main.cpp
+++ alee889/main.cpp
@@ -4,8 +4,6 @@
 #include<string>
 
 using namespace std;
-ifstream infile ;
-ofstream outfile ;
 string readFile, writeFile ;
 int numberLines = 0 ;
 
@@ -13,8 +11,8 @@ void AT() { // always taken
 	string behavior, line, ignore ;
 	int TCounter = 0 ;
 	int NTCounter = 0 ;
-
-	infile.open( readFile ) ;
+		
+	ifstream infile( readFile ) ;
 	while( getline( infile, line )) {
 		numberLines++ ;
 		stringstream s( line ) ;
@@ -22,16 +20,19 @@ void AT() { // always taken
 
 		if( behavior == "T" ) {
 			TCounter++ ;
+			// printf( "%d", TCounter ) ;
 		}
 		else if( behavior == "NT" ) {
 			NTCounter++ ;
+			// printf( "%d", NTCounter ) ;
+
 		}
 	}
 
 	infile.close() ;
-	outfile.open( writeFile ) ;
-	outfile << to_string( TCounter ) + "," + to_string( numberLines ) << endl ;
-	outfile << to_string( NTCounter ) + "," + to_string( numberLines ) << endl ;
+	ofstream outfile( writeFile ) ;
+	outfile << to_string( TCounter ) + "," + to_string( numberLines ) + ";" << endl ;
+	outfile << to_string( NTCounter ) + "," + to_string( numberLines ) + ";" << endl ;
 	outfile.close() ;
 }
 
@@ -41,6 +42,8 @@ int main( int argc, char *argv[]) {
 	unsigned long long target;
 	readFile = argv[ 1 ] ;
 	writeFile = argv[ 2 ] ;
+
+	AT() ;
 
 	/*
 	infile.open( "test_input.txt" );
