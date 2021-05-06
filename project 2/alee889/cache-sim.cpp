7574dd1e4ec4dac86815773e72761548cb464e4b
unknown <alee889@binghamton.edu>
2021-05-06 01:17:32 -0400

    changes

--- project 2/alee889/cache-sim.cpp
+++ project 2/alee889/cache-sim.cpp
@@ -9,29 +9,14 @@ using namespace std;
 string readFile, writeFile ;
 int numberLines = 0 ;
 
+
+//======================== direct mapped =====================
 void dMap() {
 	string line, type ;
 	unsigned long long addr ;
 	int dBlocks[ 4 ] = { 32, 128, 512, 1024 } ;
 	int arrCaches[ 4 ][ 1024 ] ;
-	int associativities[ 4 ] = { 2, 4, 8, 16 } ;
-	int sASets[ 4 ] = { 256, 128, 64, 32 } ;
 	int dMapHits[ 4 ] = { 0, 0, 0, 0 } ;
-	int sAHits[ 4 ] = { 0, 0, 0, 0 } ;
-
-	vector<vector<int>> twoAssoc( 256, vector<int> ( 2, 0 )) ;
-	vector<vector<int>> fourAssoc( 128, vector<int> ( 4, 0 )) ;
-	vector<vector<int>> eightAssoc( 64, vector<int> ( 8, 0 )) ;
-	vector<vector<int>> sixteenAssoc( 32, vector<int> ( 16, 0 )) ;
-	vector<vector<vector<int>>*> sACaches{ &twoAssoc, &fourAssoc, &eightAssoc, &sixteenAssoc } ;
-	vector<vector<int>> twoALRU( 256, vector<int> ( 2, 0 ));
-	vector<vector<int>> fourALRU( 128, vector<int> ( 4, 0 )) ;
-	vector<vector<int>> eightALRU( 64, vector<int> ( 8, 0 )) ;
-	vector<vector<int>> sixteenALRU( 32, vector<int> ( 16, 0 )) ;
-	vector<vector<vector<int>>*> sALRUs{ &twoALRU, &fourALRU, &eightALRU, &sixteenALRU } ;
-	
-	// vector<vector<int>>* a = sACaches[ 1 ] ;
-	// cout << to_string( (*a)[ 125 ][ 2 ]) << endl ;
 	
 	ifstream infile( readFile ) ;
 	while( getline( infile, line )) {
@@ -40,7 +25,6 @@ void dMap() {
 		
 		numberLines++ ;
 
-		//======================== direct mapped =====================
 		for( int i = 0; i < 4; i++ ) {
 			unsigned long long index = ( addr / 32 ) % dBlocks[ i ] ;
 			if( arrCaches[ i ][ index ] != ( addr / 32 )) {
@@ -50,36 +34,6 @@ void dMap() {
 				dMapHits[ i ]++ ;
 			}
 		}
-		//============================================================
-
-		//=====================set associative========================
-		for( int i = 0; i < 4; i++ ) {
-			unsigned long long index = ( addr / 32 ) % sASets[ i ] ;
-			bool hit = false ;
-			int minLRU = 0;
-			vector<vector<int>>* x = sACaches[ i ] ;
-			vector<vector<int>>* y = sALRUs[ i ] ;
-
-			for( int j = 0; j < associativities[ i ]; j++ ) {
-				if((*x)[ index ][ j ] == ( addr / 32 ) * 32 ) {
-					sAHits[ i ]++ ;
-					(*y)[ index ][ j ]++ ;
-					hit = true ;
-				}
-			}
-
-			if( !hit ) {
-				for( int j = 0; j < associativities[ i ]; j++ ) {
-					if((*y)[ index ][ j ] < (*y)[ index ][ minLRU ]) {
-						minLRU = j ;
-					}
-				}
-				(*y)[ index ][ minLRU ]++ ;
-				(*x)[ index ][ minLRU ] = ( addr / 32 ) * 32 ;
-			}
-		}
-		//============================================================
-
 	}
 	
 	infile.close() ;
@@ -88,11 +42,46 @@ void dMap() {
 		outfile << to_string( dMapHits[ i ]) + "," + to_string( numberLines ) + "; " ;
 	}
 	outfile << to_string( dMapHits[ 3 ]) + "," + to_string( numberLines ) + ";" << endl ;
-	for( int i = 0; i < 3; i++ ) {
-		outfile << to_string( sAHits[ i ]) + "," + to_string( numberLines ) + "; " ;
-	}
-	outfile << to_string( sAHits[ 3 ]) + "," + to_string( numberLines ) + ";" << endl ;
 	outfile.close() ;
+}
+
+//=========================set assoc ==================================
+int sAssoc( int associativity ) {
+	string line, type ;
+	unsigned long long addr ;
+	int numHits = 0 ;
+
+	int numSets = ( 16 * 1024 )/( associativity * 32 ) ;
+	vector<vector<int>> cache( numSets, vector<int>( associativity, 0 )) ;
+	vector<vector<int>> lru( numSets, vector<int>( associativity, 0 )) ;
+	
+	ifstream infile( readFile ) ;
+	while( getline( infile, line )) {
+		stringstream s( line ) ;
+		s >> type >> hex >> addr ;
+
+		int index = ( addr / 32 ) % numSets ;
+		bool found = false ;
+		int minIndex = 0 ;
+
+		for( int i = 0; i < associativity; i++ ) {
+			if( lru[ index ][ i ] < lru[ index ][ minIndex ]) {
+				minIndex = i ;
+			}
+			if( cache[ index ][ i ] == addr / 32 ) {
+				found = true ;
+				numHits++ ;
+				lru[ index ][ i ]++ ;
+			}
+		}
+
+		if( !found ) {
+			lru[ index ][ minIndex ]++ ;
+			cache[ index ][ minIndex ] = addr / 32 ;
+		}
+	}
+
+	return( numHits ) ;
 }
 
 int main( int argc, char *argv[]) {
@@ -100,6 +89,11 @@ int main( int argc, char *argv[]) {
 	writeFile = argv[ 2 ] ;
 
 	dMap() ;
+	ofstream outfile( writeFile, fstream::app ) ;
+	outfile << to_string( sAssoc( 2 )) + "," + to_string( numberLines ) + "; " ;
+	outfile << to_string( sAssoc( 4 )) + "," + to_string( numberLines ) + "; " ;
+	outfile << to_string( sAssoc( 8 )) + "," + to_string( numberLines ) + "; " ;
+	outfile << to_string( sAssoc( 16 )) + "," + to_string( numberLines ) + "; " ;
 
 	return 0;
 }
